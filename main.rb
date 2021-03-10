05bd6c08d2c3de18d7287dfb87c377bfad455be6
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2021-03-10 17:09:43 +1100

    css of website largely done

--- main.rb
+++ main.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
 require 'bcrypt'
 
-require_relative 'db/lib'
-
-
 if development?
   require 'sinatra/reloader'
   require 'pry'
 end
+
+require_relative 'db/lib'
+
+enable(:sessions)
+
 
 get '/' do
 
@@ -18,6 +20,29 @@ get '/' do
   }
 end
 
+get '/about' do
+  erb :about
+end
+
+get '/foods/new' do
+  erb :new_food
+end
+
+post '/foods' do
+
+  run_sql("INSERT INTO food(food_type, image_url, location_description) VALUES($1, $2, $3);", 
+  [
+    params[:food_type],
+    params[:image_url],
+    params[:location_description]
+  ])
+
+  redirect '/'
+end
+
+get '/login' do
+  erb :login
+end
 
 
 
